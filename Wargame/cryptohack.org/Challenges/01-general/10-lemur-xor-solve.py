4169b392b21fa4e8c5780fbd02f6c629ff73c42f
swollows <swollows9081@gmail.com>
2026-03-11 07:19:55 +0900

    Add some cryptohack.org write-ups

--- Wargame/cryptohack.org/Challenges/01-general/10-lemur-xor-solve.py
+++ Wargame/cryptohack.org/Challenges/01-general/10-lemur-xor-solve.py
@@ -1,11 +1,21 @@
-import PIL.Image as pilimg
 import numpy as np
+from PIL import Image
 
-# Read flag.png
-img_flag = pilimg.open("./10-lemur-xor-img/flag.png")
+# Open images
+im1 = Image.open("./10-lemur-xor-img/flag.png")
+im2 = Image.open("./10-lemur-xor-img/lemur.png")
 
-# Read lemur.png
-img_lemur = pilimg.open("./10-lemur-xor-img/lemur.png")
+arr1 = np.array(im1)
+arr2 = np.array(im2)
 
-print(img_flag.shape)
-print(img_lemur.shape)
+dim = arr1.shape
+xor12 = np.zeros((dim[0], dim[1], dim[2]))
+
+for i in range(0, dim[0]):
+    for j in range(0, dim[1]):
+        for h in range(0, dim[2]):
+            xor12[i, j, h] = arr1[i, j, h] ^ arr2[i, j, h]
+
+xorImage = Image.fromarray(xor12.astype(np.uint8))
+
+xorImage.show()
